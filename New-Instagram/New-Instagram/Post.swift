1eaba9ca22c80f4ae3673e99553638bf1e46ea64
user144731 <user144731@74-80-245-6.ic2net.net>
2018-10-03 11:12:25 -0700

    user can now choose and post a photo and user is not persisted

---
+++ New-Instagram/New-Instagram/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  New-Instagram
+//
+//  Created by user144731 on 10/3/18.
+//  Copyright © 2018 rbradley. All rights reserved.
+//
+
+import UIKit
+
+class Post: PFObject {
+
+}
