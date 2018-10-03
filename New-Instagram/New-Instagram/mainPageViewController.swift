7d19359070a444148fbc5f33dda5c9036f36d6b6
user144731 <user144731@74-80-245-6.ic2net.net>
2018-10-02 18:26:53 -0700

    pushing before finish pictures

---
+++ New-Instagram/New-Instagram/mainPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  mainPageViewController.swift
+//  New-Instagram
+//
+//  Created by user144731 on 10/2/18.
+//  Copyright © 2018 rbradley. All rights reserved.
+//
+
+import UIKit
+
+class mainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
